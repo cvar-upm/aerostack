dd0537168e6eca566e4b8d09e89315b41755c0d1
joselusl <jl.sanchez@upm.es>
2014-09-23 17:13:45 +0200

    configuration options

--- initSubmodules.sh
+++ initSubmodules.sh
@@ -2,12 +2,30 @@
 echo "Configuring stack submodules .."
 
 
-droneMsgs=true
+#Reading configuration of which submodules include
+. configSubmodules.cfg
 
 
 
-
-if $droneMsgs
+#Loop for git submodule init
+if [[ ${droneMsgsROS}  && $droneMsgsROS = true ]]
 	then
 		git submodule init stack/droneStackBasis/droneMsgsROS/
+		echo " -stack/droneStackBasis/droneMsgsROS added"
 fi
+
+
+
+
+if [[ ${okto_driver}  && $okto_driver = true ]]
+	then
+		git submodule add -b catkin https://bitbucket.org/Vision4UAV/okto_driver.git ./extStackCVG/okto_driver
+		echo " -extStackCVG/okto_driver added"
+
+fi
+
+
+
+
+#git submodule update
+git submodule update

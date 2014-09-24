8e84dd2c4e2d9131e68eb7d91a511294370594ec
joselusl <jl.sanchez@upm.es>
2014-09-24 23:05:32 +0200

    all submodules added

--- expectScript.sh
+++ expectScript.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/expect -b
+#!/usr/bin/expect
 #Tells interpreter where the expect program is located.  This may need adjusting according to
 #your specific environment.  Type ' which expect ' (without quotes) at a command prompt
 #to find where it is located on your system and adjust the following line accordingly.
@@ -8,17 +8,25 @@ set gitSubmodule [lindex $argv 0]; # Grab the second command line parameter
 set bitbucketUsername [lindex $argv 1]; # Grab the second command line parameter
 set bitbucketPassword [lindex $argv 2]; # Grab the third command line parameter
 
+#turn off stdout
+#log_user 0
+
+#set timeo
+
 #Do the command
-spawn git submodule update $gitSubmodule
+#spawn git submodule update $gitSubmodule
+#if you don't want to use git submodule init
+spawn git submodule update --init $gitSubmodule
 #Wait for a response
+#"Username for 'https://bitbucket.org': " {
 expect {
-"Username for 'https://bitbucket.org': "
-	{
+"Username for 'https://bitbucket.org': " {
 	send "$bitbucketUsername\n"
 	expect "Password for 'https://$bitbucketUsername@bitbucket.org': "
 	send "$bitbucketPassword\n"
 	}
-""
+
+#"" { send "aqui\n"}
 }
 # 
 #The interact command is part of the expect script, which tells the script to hand off control to the user.

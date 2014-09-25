16200ea6c9dcade29bc5262a6aa0bd9aac4b22b9
joselusl <jl.sanchez@upm.es>
2014-09-25 17:08:56 +0200

    working with branches

--- updateFileInAllBranches.sh
+++ updateFileInAllBranches.sh
@@ -7,9 +7,9 @@ COMMIT_MSG=$2
 branches=()
 eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 for branch in "${branches[@]}"; do
-  if [[ "${branch}" != "master" ]]; then
-    git checkout ${branch}
-    git checkout master -- $FILE_TO_UPDATE 
-	git commit -m $COMMIT_MSG       
-  fi
+	if [[ "${branch}" != "master" ]]; then
+	    	git checkout ${branch}
+	    	git checkout master -- $FILE_TO_UPDATE 
+		git commit -m $COMMIT_MSG
+	fi
 done

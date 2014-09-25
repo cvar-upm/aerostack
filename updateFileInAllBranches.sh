03938fe6a02ed6b30855b747e8fd6e1fb2889e8d
joselusl <jl.sanchez@upm.es>
2014-09-25 18:16:58 +0200

    updating script to update files in all branches

--- updateFileInAllBranches.sh
+++ updateFileInAllBranches.sh
@@ -4,18 +4,28 @@ FILE_TO_UPDATE=$1
 COMMIT_MSG=$2
 
 
+
 branches=()
 eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 
+curr_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 
 for branch in "${branches[@]}"; do
 	branch_filt=${branch##"refs/heads/"}
 
-	if [[ "${branch_filt}" != "master" ]]; then
-		echo $branch_filt
+	if [[ "${branch_filt}" != $curr_branch ]]; then
+		echo "->Updating file $FILE_TO_UPDATE in branch $branch_filt, from branch $curr_branch"
+		#echo $branch_filt
+		#echo $FILE_TO_UPDATE
+		#echo $COMMIT_MSG
 	    	git checkout ${branch_filt}
-	    	git checkout master -- $FILE_TO_UPDATE 
-		git commit $FILE_TO_UPDATE -m $COMMIT_MSG
+	    	git checkout $curr_branch -- $FILE_TO_UPDATE 
+		git add $FILE_TO_UPDATE
+		git commit -m "$COMMIT_MSG"
+		git push origin ${branch_filt}
 	fi
 done
+
+#return to current branch
+git checkout ${curr_branch}

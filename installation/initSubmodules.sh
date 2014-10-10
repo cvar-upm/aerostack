64dbc7f062ba591d818bf38f2dbf4e5fafb92728
Jose Luis Sanchez Lopez <joseluis.sanlop@gmail.com>
2014-10-10 15:37:26 +0000

    initSubmodules.sh edited online with Bitbucket

--- installation/initSubmodules.sh
+++ installation/initSubmodules.sh
@@ -650,7 +650,7 @@ if [[ ${referenceFrames}  && $referenceFrames = true ]]
 		echo " -Added package in: $MODULE_PATH"
 fi
 
-#droneMsgsROS
+#videoPublisherROSModule
 if [[ ${videoPublisherROSModule}  && $videoPublisherROSModule = true ]]
 	then
 		MODULE_PATH=stack/utils/videoPublisherROSModule

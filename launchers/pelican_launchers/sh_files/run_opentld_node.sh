c0a9e1a0720e2b5cb8d2ad641b187a512381508f
Hriday <hriday.bavle@gmail.com>
2015-08-26 13:02:01 +0200

    adding openTLD sh files for pelican

---
+++ launchers/pelican_launchers/sh_files/run_opentld_node.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd $DRONE_STACK
+source setup.sh
+roslaunch ${DRONE_STACK}/launchers/pelican_launchers/launch_files/opentld_for_IBVSController.launch --wait drone_id_int:="0" drone_id_namespace:="drone0"

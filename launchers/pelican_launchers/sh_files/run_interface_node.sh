5a36c39cca0e43eec93b503ed724bac8fd8526a7
Hriday <hriday.bavle@gmail.com>
2015-09-01 13:47:16 +0200

    adding rectified image for droneInterface

--- launchers/pelican_launchers/sh_files/run_interface_node.sh
+++ launchers/pelican_launchers/sh_files/run_interface_node.sh
@@ -2,5 +2,5 @@
 
 cd $DRONE_STACK
 source setup.sh mastermind
-roslaunch droneInterfaceROSModule droneInterface_jp_ROSModule.launch --wait drone_id_int:="0" drone_id_namespace:="drone0" 
+roslaunch ${DRONE_STACK}/launchers/pelican_launchers/launch_files/droneInterface_jp_ROSModule.launch --wait drone_id_int:="0" drone_id_namespace:="drone0" 
 

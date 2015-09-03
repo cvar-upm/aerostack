3f7f885c4cc1011101fabd072ca30f7a245adb77
Hriday <hriday.bavle@gmail.com>
2015-09-03 12:43:55 +0200

    adding republisher node to convert compressed image to raw image

--- launchers/pelican_launchers/sh_files/run_opentld_node.sh
+++ launchers/pelican_launchers/sh_files/run_opentld_node.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd $DRONE_STACK
-source setup.sh
+source setup.sh mastermind
 roslaunch ${DRONE_STACK}/launchers/pelican_launchers/launch_files/opentld_for_IBVSController.launch --wait drone_id_int:="0" drone_id_namespace:="drone0"

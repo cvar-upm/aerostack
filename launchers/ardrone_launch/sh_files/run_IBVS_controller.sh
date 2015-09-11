8c0472506adf60cf4b3711456ade55461e5fc388
Hriday <hriday.bavle@gmail.com>
2015-09-11 12:40:05 +0200

    updating some packages

--- launchers/ardrone_launch/sh_files/run_IBVS_controller.sh
+++ launchers/ardrone_launch/sh_files/run_IBVS_controller.sh
@@ -2,4 +2,5 @@
 
 cd $DRONE_STACK
 source setup.sh
-roslaunch droneIBVSControllerROSModule DroneIBVSControllerROSModule.launch --wait drone_id_int:="1" drone_id_namespace:="drone1"
+roslaunch droneIBVSControllerROSModule DroneIBVSControllerROSModule.launch --wait drone_id_int:="1" drone_id_namespace:="drone1"&
+roslaunch droneTrackerEyeROSModule droneTrackerEyeROSModule.launch --wait drone_id_int:="1" drone_id_namespace:="drone1"

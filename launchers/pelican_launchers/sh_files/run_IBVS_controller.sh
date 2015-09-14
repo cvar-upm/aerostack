1809826ab27bdf80faf8cfa8c214b4e76037e605
mastermind <hriday.bavle@gmail.com>
2015-09-14 13:34:46 +0200

    changing files in the pelican_launchers

--- launchers/pelican_launchers/sh_files/run_IBVS_controller.sh
+++ launchers/pelican_launchers/sh_files/run_IBVS_controller.sh
@@ -2,4 +2,5 @@
 
 cd $DRONE_STACK
 source setup.sh
-roslaunch droneIBVSControllerROSModule DroneIBVSControllerROSModule.launch --wait drone_id_int:="0" drone_id_namespace:="drone0"
+roslaunch droneIBVSControllerROSModule DroneIBVSControllerROSModule.launch --wait drone_id_int:="0" drone_id_namespace:="drone0" &
+roslaunch droneTrackerEyeROSModule droneTrackerEyeROSModule.launch --wait drone_id_int:="0" drone_id_namespace:="drone0"

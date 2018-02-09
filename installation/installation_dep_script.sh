6bebf7085b9760bb18f8191ea98fc5bc475d790f
Alberto Camporredondo <alberto.camporredondop@alumnos.upm.es>
2018-02-09 11:51:47 +0100

    New version of Aerostack 2.1

--- installation/installation_dep_script.sh
+++ installation/installation_dep_script.sh
@@ -33,7 +33,7 @@ sudo apt-get install libudev-dev
 sudo apt-get install libiw-dev
 
 if [ "$ROS_DISTRO" = "kinetic" ]
-then 
+then
 	echo "----------------------------"
 	echo "Install driver common required in kinectic"
 	echo "----------------------------"
@@ -72,7 +72,7 @@ echo "---------------------------"
 echo "Installing Sound Play & Dependencies"
 echo "---------------------------"
 sudo apt-get install ros-$ROS_DISTRO-audio-common
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev 
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
 
 echo "---------------------------"
 echo "Installing Voices for Sound Play"
@@ -91,6 +91,10 @@ echo "------------------------------------------------------"
 sudo apt-get install libqwt-headers
 sudo apt-get install libqwt-qt5-dev
 sudo apt-get install libzbar-dev
+echo "------------------------------------------------------"
+echo "Installing xfce4-terminal"
+echo "------------------------------------------------------"
+sudo apt-get install xfce4-terminal -y
 echo "------------------------------------------------------"
 echo "Installing Ueyecamera drivers"
 echo "------------------------------------------------------"
@@ -128,4 +132,3 @@ echo "Installing All ROS dependencies"
 echo "------------------------------------------------------"
 rosdep update
 rosdep install -r --from-paths ${AEROSTACK_WORKSPACE} --ignore-src --rosdistro=$ROSDISTRO
-
--- setup.sh
+++ setup.sh
@@ -12,13 +12,14 @@ echo "-Setting ROS workspace"
 if [ -z $NETWORK_ROSCORE ] # Check if NETWORK_ROSCORE is NULL
   then
     echo "-Setting roscore in localhost"
-    export ROS_MASTER_URI=http://localhost:11311
-    export ROS_IP=localhost
-    export ROS_HOSTNAME=localhost
+    host_name=`cat /etc/hostname`
+    export ROS_MASTER_URI=http://$host_name:11311
+    export ROS_IP=$host_name
+    export ROS_HOSTNAME=$host_name
   else
     echo "-Setting roscore in $NETWORK_ROSCORE"
     export ROS_MASTER_URI=http://$NETWORK_ROSCORE:11311
-    OWN_IP=$(ifconfig | grep -Eo "192\.168\.[0-9]{1,3}\.[0-9]{1,3}" | head -n1)
+    OWN_IP=$(ifconfig | grep -Eo "[0-9]{3}\.[0-9]{2,3}\.[0-9]{1,3}\.[0-9]{1,3}" | head -n1)
     echo "-My IP in network (please check!)="$OWN_IP
     export ROS_IP=$OWN_IP
     export ROS_HOSTNAME=$OWN_IP

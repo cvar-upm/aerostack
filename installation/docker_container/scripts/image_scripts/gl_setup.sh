f5d18a276f65e1b24099332069814e3e6cf4d0ee
Daniel Del Olmo <daniel.olmo.orejana@alumnos.upm.es>
2019-01-30 10:30:17 +0100

    new docker_container repository

--- installation/docker_container/scripts/image_scripts/gl_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "downloading nvidia driver... $GL_VERSION"
-if test "$GL_VERSION" != "-1"; then
-  curl http://us.download.nvidia.com/XFree86/Linux-x86_64/"$GL_VERSION"/NVIDIA-Linux-x86_64-"$GL_VERSION".run -o /tmp/NVIDIA-DRIVER.run
-  sh /tmp/NVIDIA-DRIVER.run -a -N --ui=none --no-kernel-module
-  rm /tmp/NVIDIA-DRIVER.run
-fi
--- installation/docker_container/scripts/image_scripts/ros_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-shell=${SHELL:-"/bin/bash"}
-shell=`basename $shell`
-# setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.$shell"
-unset $shell
-set +e
-exec "$@"

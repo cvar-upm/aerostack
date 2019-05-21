88e8c0be4db03a622bbc118b262b321674dc7f84
alejodosr <alejandro.rramos@alumnos.upm.es>
2019-05-21 15:16:26 +0200

    developed function included and specific commit selector included

--- installation/aerostack_project_selector.py
+++ installation/aerostack_project_selector.py
@@ -42,34 +42,33 @@ class ScrollableFrame(tk.Frame):
 
 if __name__ == '__main__':
     # Read arguments
+    devel=0
     if len(sys.argv) < 3:
-        print("Correct syntax: 'python aerostack_project_selector.py <root_aerostack_path> <project_folder>'")
+        print("ERROR: Correct syntax: 'python aerostack_project_selector.py <root_aerostack_path> <project_folder>'")
         exit()
-
-    root = tk.Tk()
-    checkbox_pane = ScrollableFrame(root, bg='#444444')
-    checkbox_pane.pack(expand="true", fill="both")
+        
+    if (len(sys.argv)>3 and sys.argv[3]=="-d"):
+        devel=1
+    if(devel):
+        root = tk.Tk()
+        checkbox_pane = ScrollableFrame(root, bg='#444444')
+        checkbox_pane.pack(expand="true", fill="both")
 
     # Get submodules list
     GITMODULES_ROOTDIR = sys.argv[1]
     PROJECT_CONFIG = sys.argv[2]
 
+
+    print("INFO: Project directory: "+ GITMODULES_ROOTDIR + PROJECT_CONFIG)
+
     # Make a .gitmodules backup
     copyfile(GITMODULES_ROOTDIR + ".gitmodules", GITMODULES_ROOTDIR + PROJECT_CONFIG + ".gitmodules_backup")
     print("INFO: Backup of .gitmodules has been created (.gitmodules_backup)")
 
-    # Include CATKIN_IGNORE in every repository
-    # for subdir, dirs, files in os.walk(GITMODULES_ROOTDIR):
-    #     for file in files:
-    #         if file == ".git":
-    #             print("Found .git: " + str(os.path.join(subdir, file)))
-    #             try:
-    #                 open(os.path.join(subdir, "CATKIN_IGNORE"), 'a').close()
-    #             except:
-    #                 pass
-
-    # If previous configuration is defined
+    print("INFO: Retrieving previous configuration if it exists...")
     prior_submodules = []
+    prior_commits = []
+    prior_commits_submodule = []
     try:
         with open(GITMODULES_ROOTDIR + PROJECT_CONFIG + ".gitmodules", 'rb') as f:
             # Read lines
@@ -81,31 +80,48 @@ if __name__ == '__main__':
                     prior_submodules.append(os.path.split(current_path)[1])
                     current_path = gt_list[i + 1].split(' ')
                     prior_paths.append(current_path[2].replace('\n', ''))
+                    # Configure the selected commit
+                    try:
+                        if "commit = " in gt_list[i + 4]:
+                            prior_commits.append(gt_list[i + 4])
+                            prior_commits_submodule.append(prior_submodules[len(prior_submodules) - 1])
+                    except:
+                        pass
     except:
         pass
-
+    print("INFO: Reading submodules info...")
     with open(GITMODULES_ROOTDIR + '.gitmodules', 'rb') as f:
         # Read lines
         gt_list = f.readlines()
         paths = []
         submodules = []
         prior_var = []
+        commits = []
         for i, row in enumerate(gt_list):
             if row.startswith("[submodule"):
+                # Find name of the submodule and activate prior activation for this project
                 current_path = re.findall(r'"(.*?)"', row)[0]
                 current_submodule = os.path.split(current_path)[1]
                 submodules.append(current_submodule)
                 prior_var.append(0)
                 current_path = gt_list[i + 1].split(' ')
                 paths.append(current_path[2].replace('\n', ''))
+                commits.append("")
                 if len(prior_submodules) > 0:
+                    # Activate those that where present
                     for prior_submodule in prior_submodules:
                         if prior_submodule == current_submodule:
                             prior_var[len(prior_var) - 1] = 1
 
+                    # Include specific commits
+                    for c, prior_submodule in enumerate(prior_commits_submodule):
+                        if prior_submodule == current_submodule:
+                            commits[len(commits) - 1] = prior_commits[c]
+                            print("INFO: Found specific commit in module " + current_submodule + " (" + commits[len(commits) - 1] + ")")
 
     def var_states():
         # Remove all the CATKIN_IGNORE found
+        print("INFO: Removing all CATKIN_INGNORE...")
         for subdir, dirs, files in os.walk(GITMODULES_ROOTDIR):
             for file in files:
                 if file == "CATKIN_IGNORE":
@@ -116,6 +132,7 @@ if __name__ == '__main__':
                         pass
 
         # Try removing previous generated file
+        print("INFO: Removing previous generated files if they exist...")
         try:
             os.remove(GITMODULES_ROOTDIR + PROJECT_CONFIG + '.gitmodules')
         except:
@@ -130,7 +147,7 @@ if __name__ == '__main__':
             os.remove(GITMODULES_ROOTDIR + 'git_submodule_sequence.sh')
         except:
             pass
-
+        print("INFO: Managing CATKIN_IGNORE and creating git_submodule_sequence.sh...")
         with open(GITMODULES_ROOTDIR + '.gitmodules', 'rb') as f:
             # Read lines
             gt_list = f.readlines()
@@ -144,9 +161,13 @@ if __name__ == '__main__':
                         # print(os.path.join(GITMODULES_ROOTDIR, paths[k - 1] + "/CATKIN_IGNORE"))
                         os.mknod(os.path.join(GITMODULES_ROOTDIR, paths[k - 1] + "/CATKIN_IGNORE"))
                     except:
-                        print("File already exists: " + os.path.join(GITMODULES_ROOTDIR, paths[k - 1] + "/CATKIN_IGNORE"))
-
-                    if var_list[k - 1].get() == 1:
+                        print("INFO: File already exists: " + os.path.join(GITMODULES_ROOTDIR, paths[k - 1] + "/CATKIN_IGNORE"))
+                    val=0
+                    if (devel):
+                        val=var_list[k - 1].get()
+                    else:
+                        val=var_list[k - 1]  
+                    if val == 1:
                         # Include file in new .gitmodules
                         with open(GITMODULES_ROOTDIR + PROJECT_CONFIG + '.gitmodules', 'a') as the_file:
                             the_file.write(gt_list[j])
@@ -163,12 +184,23 @@ if __name__ == '__main__':
                                 # print(gt_list[j + 3])
                                 with open(GITMODULES_ROOTDIR + PROJECT_CONFIG + '.gitmodules', 'a') as the_file:
                                     the_file.write(gt_list[j + 3])
+                            if commits[k - 1] != "":
+                                with open(GITMODULES_ROOTDIR + PROJECT_CONFIG + '.gitmodules', 'a') as the_file:
+                                    the_file.write(commits[k - 1])
                         except:
                             pass
 
                         # Include in output script
                         with open(GITMODULES_ROOTDIR + PROJECT_CONFIG + 'git_submodule_sequence.sh', 'a') as the_file:
                             the_file.write("git submodule update --init " + paths[k - 1] + "\n")
+
+                        if commits[k - 1] != "":
+                            current_commit = commits[k - 1].split(' ')
+                            current_commit = current_commit[2].replace('\n', '')
+                            with open(GITMODULES_ROOTDIR + PROJECT_CONFIG + 'git_submodule_sequence.sh', 'a') as the_file:
+                                the_file.write("cd " + paths[k - 1] + "\n")
+                                the_file.write("git checkout " + current_commit + "\n")
+                                the_file.write("cd -" + "\n")
 
                         # Remove previously included CATKIN_IGNORE
                         os.remove(os.path.join(GITMODULES_ROOTDIR, paths[k - 1] + "/CATKIN_IGNORE"))
@@ -178,21 +210,22 @@ if __name__ == '__main__':
                  GITMODULES_ROOTDIR + "git_submodule_sequence.sh")
 
         print("INFO: All modules but selected have been put into CATKIN_IGNORE")
-        print("INFO: Please, proceed to execute gitsubmodule_sequence.sh to update selected submodules")
-        print("INFO: Process executed successfully")
-
-
+        print("INFO: Project set successfully")
+        print("INFO: Press Quit to update selected submodules")
 
     var_list = []
+    if(devel):
+        for i, submodule in enumerate(submodules):
+            var_list.append(tk.IntVar())
+            var_list[i].set(prior_var[i])
+            tk.Checkbutton(checkbox_pane.interior, text=submodule, variable=var_list[i]).grid(row=(i + 1), column=0)
 
-    for i, submodule in enumerate(submodules):
-        var_list.append(tk.IntVar())
-        var_list[i].set(prior_var[i])
-        tk.Checkbutton(checkbox_pane.interior, text=submodule, variable=var_list[i]).grid(row=(i + 1), column=0)
+        tk.Button(checkbox_pane.interior, text='Generate', command=var_states).grid(row=(i + 2), column=0)
+        tk.Button(checkbox_pane.interior, text='Quit', command=root.quit).grid(row=(i + 3), column=0)
 
-    tk.Button(checkbox_pane.interior, text='Generate', command=var_states).grid(row=(i + 2), column=0)
-    tk.Button(checkbox_pane.interior, text='Quit', command=root.quit).grid(row=(i + 3), column=0)
-
-    root.mainloop()
+        root.mainloop()
+    else:
+        var_list =prior_var
+        var_states()
 
 
